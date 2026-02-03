ae31e6f7306220068b7266f63464681dd4283cfb
epick32851 <tamjidhossain596@gmail.com>
2026-02-03 20:20:44 +0600

    added

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const serviceAccount = JSON.parse(decoded);
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://lonelink-d3167.web.app"],
     credentials: true,
   }),
 );
